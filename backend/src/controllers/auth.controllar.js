ed7de70d015fc21e2cc3771277a870734bac6154
shounokdatta <91007733+shounokdatta@users.noreply.github.com>
2025-06-26 01:24:25 +0530

    lastUpdate

--- backend/src/controllers/auth.controllar.js
+++ backend/src/controllers/auth.controllar.js
@@ -1,6 +1,18 @@
+import User from "../modules/user.model.js";
+
 export const signup = (req, res) => {
-    res.send("Signup page");
+    const { email, fullname, password } = req.body;
+try{
+         if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });    
+         }
+         const user =awit User.findOne({email})
+
+         if(user) return res.status(400).json({ message: "User already exists" });
+} catch(err){
+
 }
+};
 export const login = (req, res) => {
     res.send("login page");
 }
---
+++ backend/src/lib/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection failed", err);
+  }
+};
+
+export default connectDB;
