15943cde6b6454797c7cb0b37d51b64defd9a485
shounokdatta <91007733+shounokdatta@users.noreply.github.com>
2025-07-06 23:33:17 +0530

    commit

--- frontend/src/store/use.store.js
+++ frontend/src/store/use.store.js
@@ -1,13 +1,29 @@
-import {create} from 'zustand';
-import {axiosInstance} from '../lib/axios';
-
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios';
 
 export const useAuthStore = create((set) => ({
   authUser: null,
-  checkAuth: () => {
-    // Example async call
-    setTimeout(() => {
-      set({ authUser: { name: 'Shounok Datta' } });
-    }, 1000);
+  isSigningup: false,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get('/check');
+      set({ authUser: res.data.user });
+    } catch (err) {
+  console.error('Signup failed:', err.response?.data || err.message);
+}
+  },
+
+  SignUp: async (formData) => {
+    set({ isSigningup: true });
+    try {
+      const res = await axiosInstance.post('/auth/register', formData);
+      console.log('Signup success:', res.data);
+      set({ authUser: res.data.user || res.data });
+    } catch (err) {
+      console.error('Signup failed:', err.response?.data || err.message);
+    } finally {
+      set({ isSigningup: false });
+    }
   },
 }));
