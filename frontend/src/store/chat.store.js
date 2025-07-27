1259811e3e9a73fc8b480c6d46b19e208289d8e0
shounokdatta <91007733+shounokdatta@users.noreply.github.com>
2025-07-27 13:25:16 +0530

    project completed

--- frontend/src/store/chat.store.js
+++ frontend/src/store/chat.store.js
@@ -1,7 +1,6 @@
 import {create} from 'zustand';
 import toast from 'react-hot-toast';
 import {axiosInstance} from "../lib/axios";
-import { disconnect } from 'mongoose';
 
 
 import {useAuthStore} from "./use.store"
