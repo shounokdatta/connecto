4c7a40776c2166e773df6b63a7f8130cd2681b9f
Sk Kaif Uddin <kaifsk969@gmail.com>
2025-08-06 00:13:13 +0530

    vite file change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,21 @@
+// filepath: d:\web dev\connecto\connecto\frontend\vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: true
+  }
 })
