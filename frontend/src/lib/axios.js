997e68348a5e0e7fc218f4133078923f57d385ab
shounokdatta <91007733+shounokdatta@users.noreply.github.com>
2025-08-06 18:16:05 +0530

    frontend_fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/',
+    baseURL: 'http://localhost:5000/api/',
     withCredentials: true,
     headers: {
     'Content-Type': 'application/json',
