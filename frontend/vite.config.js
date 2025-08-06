3337e969a9ee39a75eadc16738b3d89826ae086c
shounokdatta <91007733+shounokdatta@users.noreply.github.com>
2025-08-06 23:48:52 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    outDir: 'dist', // this is default for Vite
+    assetsDir: 'assets',
+  },
 })
