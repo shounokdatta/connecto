b9e01cad91c30359f2ec686d133bc1ab2e39785d
shounokdatta <91007733+shounokdatta@users.noreply.github.com>
2025-08-14 19:59:17 +0530

    frontend_update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://connecto-pi.vercel.app/api/',
+    baseURL: 'https://connecto-bu6b.vercel.app/api/',
     withCredentials: true,
     headers: {
     'Content-Type': 'application/json',
